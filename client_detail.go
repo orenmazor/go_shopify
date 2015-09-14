9a542651b1ef3a49b7ca4dc5644e4c0f10678870
Oren Mazor <oren.mazor@gmail.com>
2015-09-14 13:54:26 -0400

    Update client_detail.go

--- client_detail.go
+++ client_detail.go
@@ -6,11 +6,11 @@ type ClientDetail struct {
   
     AcceptLanguage string `json:"accept_language"`
   
-    BrowserHeight string `json:"browser_height"`
+    BrowserHeight int64 `json:"browser_height"`
   
     BrowserIp string `json:"browser_ip"`
   
-    BrowserWidth string `json:"browser_width"`
+    BrowserWidth int64 `json:"browser_width"`
   
     SessionHash string `json:"session_hash"`
   
